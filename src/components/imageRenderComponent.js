4bf6e6d48c5e2613b997b0a8156a1c18dd7a38a6
hai.lv <vanhaiit2018@gmail.com>
2019-03-22 11:09:36 +0700

    update code new

--- src/components/imageRenderComponent.js
+++ src/components/imageRenderComponent.js
@@ -2,12 +2,12 @@ import React, { Component } from 'react';
 import initPropeties from '../utils/defaultPropetiesClothes';
 
 class ImageRenderComponent extends Component {
-
+    isNotExists = false;
     constructor(props) {
         super(props);
         this.state = {
             prefix: true,
-            isTrenchcoat: false
+            isTrenchcoat: false,
         }
     }
 
@@ -25,9 +25,15 @@ class ImageRenderComponent extends Component {
         var result = images.map((element, index) => {
             var subUrl = "";
             if (element.element.length === 0) return false
-            element.element.forEach((e, index) => {
-                if (element.index) { if (element.index[index] !== undefined) subUrl += e + "+"; } else { subUrl += e + "+"; }
-            });
+            if (element.index) {
+                element.index.forEach(ix => {
+                    subUrl += element.element[ix] + "+";
+                });
+            } else {
+                element.element.forEach((e, index) => {
+                    subUrl += e + "+"
+                });
+            }
             subUrl = subUrl.slice(0, subUrl.length - 1);
             if (subUrl === "empty+cut_slim" || subUrl === "empty" || subUrl === "cut_slim+cut_slim" || subUrl === "empty+third" || subUrl === "cut_regular+cut_slim") return false;
             if (element.prefix === "front/" && this.state.prefix) {
@@ -45,6 +51,8 @@ class ImageRenderComponent extends Component {
     }
 
     setProperties = (style) => {
+        console.log(style);
+
         if (!style) return false;
         style.short_key === "M" ? this.setState({ isTrenchcoat: true }) : this.setState({ isTrenchcoat: false })
         var indexType; var propetiesUpdate = [];
@@ -57,11 +65,7 @@ class ImageRenderComponent extends Component {
                 indexType = 2;
                 break;
             case "waistcoat_lapel":
-                if (style.id === "waistcoat_lapel_notch"
-                    || style.id === "waistcoat_lapel_peak"
-                    || style.id === "waistcoat_lapel_round")
-                    indexType = 2;
-                else indexType = 1;
+                indexType = 2;
                 break;
             case "waistcoat_lapel_width":
                 indexType = 1;
@@ -109,34 +113,19 @@ class ImageRenderComponent extends Component {
         }
 
         if (style.short_key === "G") {
+            if (style.id === "waistcoat_lapel_peak" || style.id === "waistcoat_lapel_round" || style.id === "waistcoat_lapel_notch") this.isNotExists = false;
+            if (style.id === "waistcoat_no_lapel" || style.id === "waistcoat_piece") this.isNotExists = true;
             initPropeties.gile.value.map(p => {
+                if (style.group.length === 0 && p.key === "ao_gile") p['index'] = [0, 1, 2];
                 style.group.map(g => {
                     if (p.key === g) {
-                        if (style.id === "waistcoat_lapel_notch" || style.id === "waistcoat_lapel_peak" || style.id === "waistcoat_lapel_round") {
-                            if (style.image[g].front) {
-                                if (p.element.length === 3) {
-                                    p.element[indexType] = style.image[g].front[0]
-                                } else {
-                                    p.element.splice(1, 0, "lapel_medium")
-                                    p.element.splice(indexType, 0, style.image[g].front[0])
-                                }
-                            }
-                            if (style.image[g].back) {
-                                if (p.element.length === 3) {
-                                    p.element[indexType] = style.image[g].back[0]
-                                } else {
-                                    p.element.splice(1, 0, "lapel_medium")
-                                    p.element.splice(indexType, 0, style.image[g].back[0])
-                                }
-                            }
-                            p['index'] = style.index[g];
-                        } else {
-                            if (p.element.length === 3) p.element.splice(1, 1)
-                            p.element[indexType] = style.image[g].front ? style.image[g].front[0] : style.image[g].back[0];
-                            p['index'] = style.index[g];
-                        }
+                        p.element[indexType] = style.image[g].front ? style.image[g].front[0] : style.image[g].back[0];
+                        p['index'] = style.index[g];
                     }
                 });
+                if (this.isNotExists && p.key === "ao_gile" && p.index.length == 3) p.index.splice(1, 1);
+                if (!this.isNotExists && p.key === "ao_gile" && p.index.length == 2) p.index.splice(1, 0, 1);
+
                 propetiesUpdate.push(p);
             });
         }
@@ -149,9 +138,10 @@ class ImageRenderComponent extends Component {
                         if (g === "nep_ao_somi") indexType = 1;
                         if (g === "nep_ao_somi" && style.props_name === "shirt_fit") indexType = 0;
                         p.element[indexType] = style.image[g].front ? style.image[g].front[0] : style.image[g].back[0];
-                        p['index'] = style.index[g]
+                        p['index'] = style.index[g];
                     }
                 });
+
                 propetiesUpdate.push(p)
             });
         }
@@ -167,6 +157,7 @@ class ImageRenderComponent extends Component {
                 propetiesUpdate.push(p)
             });
         }
+        console.log(propetiesUpdate);
 
         this.setState({
             initPropeties: propetiesUpdate,
